2abc9427ed8cbcacee7c5b570a4bc8754ddadc18
Barkmannen <159003381+barkmannen@users.noreply.github.com>
2024-03-14 13:30:18 +0200

    Update layout.tsx
    Signed-off-by: Barkmannen <159003381+barkmannen@users.noreply.github.com>

--- dapp/web/app/layout.tsx
+++ dapp/web/app/layout.tsx
@@ -1,30 +1,28 @@
-import './global.css';
-import { AppLayout } from '@/components/ui/app-layout';
+import React from 'react';
+import Head from 'next/head';
 import { ClusterProvider } from '@/components/cluster/cluster-data-access';
 import { SolanaProvider } from '@/components/solana/solana-provider';
-import Head from 'next/head';
+import { AppLayout } from '@/components/ui/app-layout';
 
-export const metadata = {
+// Define metadata for the page
+const metadata = {
   title: 'bark-swap',
   description: 'Decentralized Exchange',
 };
 
-export default function RootLayout({children,}: {children: React.ReactNode; }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <>
       <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <script src="https://terminal.jup.ag/main-v2.js" data-preload></script>
       </Head>
-      <body>
-        <ClusterProvider>
-          <SolanaProvider>
-            {/* bruh moment */}
-            <AppLayout>{children}</AppLayout>
-          </SolanaProvider>
-        </ClusterProvider>
-      </body>
-    </html>
+      <ClusterProvider>
+        <SolanaProvider>
+          <AppLayout>{children}</AppLayout>
+        </SolanaProvider>
+      </ClusterProvider>
+    </>
   );
 }
