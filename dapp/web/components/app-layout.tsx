1c8cd85320ffe0f179df92884707041711e22bdf
Barkmannen <159003381+barkmannen@users.noreply.github.com>
2024-03-14 13:34:35 +0200

    Update app-layout.tsx
    Signed-off-by: Barkmannen <159003381+barkmannen@users.noreply.github.com>

--- dapp/web/components/app-layout.tsx
+++ dapp/web/components/app-layout.tsx
@@ -1,55 +1,50 @@
-// AppLayout.tsx
-
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import { ReactNode } from 'react';
+
 import Link from 'next/link';
-import { WalletButton } from '../solana/solana-provider';
-import styles from './AppLayout.module.css';
 
-interface AppLayoutProps {
-  children: ReactNode;
-  logoSrc?: string;
-}
-
-const FooterContent = () => (
-  <p>
-    © 2024 BARK Protocol. All rights reserved{' '}
-    <a
-      href="https://swap.barkprotocol.net/legals/terms-of-use"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Terms of Use
-    </a>
-  </p>
-);
-
-export function AppLayout({ children, logoSrc = '/logo-dark.svg' }: AppLayoutProps) {
-  const renderLogo = !!logoSrc;
-
+export function AppLayout({ children }: { children: ReactNode }) {
   return (
-    <div className={styles.container}>
-      <header className={`${styles.flex} ${styles.header}`}>
-        {renderLogo && (
-          <div>
-            <Link href="/" passHref>
-              <a>
-                <img className={styles.logo} src={logoSrc} alt="Bark Logo" />
-              </a>
-            </Link>
-          </div>
-        )}
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          padding: '6px 12px',
+        }}
+      >
         <div>
-          <WalletButton />
+          <Link href="/">
+            <img src="/assets/bark-dark.png" height={24} alt="BARK Logo" />
+          </Link>
         </div>
-      </header>
-      <main className={styles.main}>{children}</main>
-      {children && (
-        <footer className={styles.footer}>
-          <aside>
-            <FooterContent />
-          </aside>
-        </footer>
-      )}
+        <div>
+          <WalletMultiButton />
+        </div>
+      </div>
+      <div style={{ flexGrow: 1, padding: '6px' }}>{children}</div>
+      <footer
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: '6px 10px',
+        }}
+      >
+        <aside>
+          <p>
+            © 2024 BARK Protocol. All rights reserved{' '}
+            <a
+              href="https://swap.barkprotocol.net/legals/terms-of-use"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Terms of Use
+            </a>
+          </p>
+        </aside>
+      </footer>
     </div>
   );
 }
